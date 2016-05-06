4e7188f1156ac34ec092b0463b28be58635da061
Denis Grotsev <dgrotsev@greet-go.com>
2016-05-06 16:54:00 +0300

    add read and multithreading

--- src/main/java/kz/greetgo/loadtest/TestConsumer.java
+++ src/main/java/kz/greetgo/loadtest/TestConsumer.java
@@ -4,13 +4,17 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static java.lang.System.out;
 
@@ -19,60 +23,117 @@ import static java.lang.System.out;
  */
 public class TestConsumer {
 
-    public static void main(String[] args) throws IOException, SQLException {
-        Properties config = config();
-        Map<String, int[]> topics = TestProducer.topics(config);
+    public static void main(String[] args) throws IOException, SQLException, ExecutionException, InterruptedException {
+        Properties config = config(args);
+        String topicNames = config.getProperty("topic.names");
+        int numTopics = Integer.valueOf(config.getProperty("num.topics"));
+        int numColumns = Integer.valueOf(config.getProperty("num.columns"));
+        int numInserts = Integer.valueOf(config.getProperty("num.inserts"));
+        int numSelects = Integer.valueOf(config.getProperty("num.selects"));
+        int numSelectThreads = Integer.valueOf(config.getProperty("num.select.threads"));
+        boolean testWrite = Boolean.valueOf(config.getProperty("test.write", "true"));
 
-        KafkaConsumer<Long, String> consumer = new KafkaConsumer<>(config);
-        ArrayList<String> topicNames = new ArrayList<>(topics.keySet());
-        consumer.subscribe(topicNames);
+        KafkaConsumer<Integer, String> consumer = new KafkaConsumer<>(config);
+        List<String> topics = IntStream.range(0, numTopics).mapToObj(i -> topicNames + i).collect(Collectors.toList());
+        consumer.subscribe(topics);
 
-        String jdbcUrl = config.getProperty("jdbc.url");
-        String jdbcUsername = config.getProperty("jdbc.username");
-        String jdbcPassword = config.getProperty("jdbc.password");
-        Connection connection = DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword);
-        connection.setAutoCommit(false);
+        final String jdbcUrl = config.getProperty("jdbc.url");
+        final String jdbcUsername = config.getProperty("jdbc.username");
+        final String jdbcPassword = config.getProperty("jdbc.password");
 
-        Statement stmt = connection.createStatement();
-        for (Map.Entry<String, int[]> entry : topics.entrySet()) {
-            String createTable = createTable(entry.getKey(), entry.getValue()[0]);
-            stmt.executeUpdate(createTable);
-            //out.println(createTable);
-        }
+        final Queue<Connection> queue = new ConcurrentLinkedQueue<>();
+        ThreadLocal<Connection> conn = ThreadLocal.withInitial(() -> {
+            try {
+                Connection c = DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword);
+                c.setAutoCommit(false);
+                queue.add(c);
+                return c;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        });
 
         long start = System.currentTimeMillis();
-        while (true) {
-            ConsumerRecords<Long, String> records = consumer.poll(1000);
-            System.out.println("Count "+ records.count());
-            if (records.isEmpty()) break;
-            stmt.clearBatch();
-            for (ConsumerRecord<Long, String> record : records) {
-                String upsert = upsert(record);
-                stmt.addBatch(upsert);
-                //out.println(upsert);
+        long now;
+
+        if (testWrite) { // write
+            try (Statement stmt = conn.get().createStatement()) {
+                for (String topic : topics)
+                    stmt.executeUpdate(createTable(topic, numColumns));
+
+                while (true) {
+                    ConsumerRecords<Integer, String> records = consumer.poll(1000);
+                    System.out.println("Batch records count " + records.count());
+                    if (records.isEmpty()) break;
+                    for (ConsumerRecord<Integer, String> record : records)
+                        stmt.addBatch(upsert(record));
+                    stmt.addBatch("commit");
+                    stmt.executeBatch();
+                    stmt.clearBatch();
+                    //connection.commit();
+                    consumer.commitSync();
+                }
+
+                now = System.currentTimeMillis();
+                System.out.println("Write time: " + (now - start));
+                start = now;
             }
-
-            long now = System.currentTimeMillis();
-            System.out.println("Before commit "+ (now-start));
-
-            stmt.executeBatch();
-            connection.commit();
-            consumer.commitSync();
-
-            now = System.currentTimeMillis();
-            System.out.println("After  commit "+(now-start));
         }
 
-        stmt.close();
-        connection.close();
+        // read
+        ForkJoinPool forkJoinPool = new ForkJoinPool(numSelectThreads);
+        Long count = forkJoinPool.submit(() ->
+                (long)IntStream.range(0, numSelects).parallel().reduce(0, (acc, sel) -> {
+                    ThreadLocalRandom rnd = ThreadLocalRandom.current();
+                    try (Statement stmt = conn.get().createStatement()) {
+                        String select = select(topics, sel); // rnd.nextInt(numInserts)
+                        ResultSet rs = stmt.executeQuery(select);
+                        int subCount = 0;
+                        while (rs.next()) {
+                            int id = rs.getInt("id");
+                            if (id < 0) subCount++; // Against optimisation. It is never executed because (id >= 0)
+                            subCount++;
+                        }
+                        return acc+subCount;
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    return 0;
+                })
+        ).get();
+
+        now = System.currentTimeMillis();
+        System.out.println("Read time: " + (now - start) + ", count: " + count);
+
+        for (Connection c : queue) {
+            c.close();
+        }
     }
 
-    private static String createTable(String tableName, int columns) {
+    private static String select(List<String> tables, int i0) {
+        int i1 = i0 + 10;
+        StringBuilder sb = new StringBuilder();
+        sb.append("select * from");
+        boolean tail = false;
+        for (String table : tables) {
+            if (tail) {
+                sb.append(" left join ").append(table).append(" using (id)");
+            } else {
+                sb.append(" ").append(table);
+                tail = true;
+            }
+        }
+        sb.append(" where id between ").append(i0).append(" and ").append(i1);
+        return sb.toString();
+    }
+
+    private static String createTable(String tableName, int numColumns) {
         StringBuilder sb = new StringBuilder();
         sb.append("create table if not exists ");
         sb.append(tableName);
         sb.append(" (id int not null primary key");
-        for (int col = 0; col < columns; col++) {
+        for (int col = 0; col < numColumns; col++) {
             sb.append(", value");
             sb.append(col);
             sb.append(" varchar(30)");
@@ -81,10 +142,10 @@ public class TestConsumer {
         return sb.toString();
     }
 
-    private static String upsert(ConsumerRecord<Long, String> record) {
+    private static String upsert(ConsumerRecord<Integer, String> record) {
         StringBuilder sb = new StringBuilder();
         String topic = record.topic();
-        Long key = record.key();
+        Integer key = record.key();
         String[] cols = record.value().split("\\|");
         sb.setLength(0);
         sb.append("insert ");
@@ -96,8 +157,8 @@ public class TestConsumer {
             sb.append(col);
         }
         sb.append(") on duplicate key update ");
-        for (int col = 0; col<cols.length; col++) {
-            if (col>0) sb.append(", ");
+        for (int col = 0; col < cols.length; col++) {
+            if (col > 0) sb.append(", ");
             sb.append("value");
             sb.append(col);
             sb.append(" = ");
@@ -106,15 +167,15 @@ public class TestConsumer {
         return sb.toString();
     }
 
-    private static Properties config() throws IOException {
+    private static Properties config(String[] args) throws IOException {
         Properties config = new Properties();
-        InputStream configStream = TestProducer.class.getResourceAsStream("config.properties");
+        InputStream configStream = new FileInputStream(args[0]);// TestProducer.class.getResourceAsStream("config.properties");
         config.load(configStream);
         config.putIfAbsent("enable.auto.commit", "false");
         config.putIfAbsent("auto.commit.interval.ms", "1000");
         config.putIfAbsent("session.timeout.ms", "30000");
         config.putIfAbsent("auto.offset.reset", "earliest");
-        config.put("key.deserializer", "org.apache.kafka.common.serialization.LongDeserializer");
+        config.put("key.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
         config.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         return config;
     }
--- src/main/java/kz/greetgo/loadtest/TestProducer.java
+++ src/main/java/kz/greetgo/loadtest/TestProducer.java
@@ -3,6 +3,7 @@ package kz.greetgo.loadtest;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
@@ -17,50 +18,39 @@ import java.util.stream.Collectors;
 public class TestProducer {
 
     public static void main(String[] args) throws IOException {
-        Properties config = config();
-        Map<String, int[]> topics = topics(config);
+        Properties config = config(args);
+        String topicNames = config.getProperty("topic.names");
+        int numTopics = Integer.valueOf(config.getProperty("num.topics"));
+        int numColumns = Integer.valueOf(config.getProperty("num.columns"));
+        int numInserts = Integer.valueOf(config.getProperty("num.inserts"));
+        int numUpdates = Integer.valueOf(config.getProperty("num.updates"));
 
         // TODO drop and create partitioned topics
 
-        for (Map.Entry<String, int[]> entry : topics.entrySet()) {
-            String topic = entry.getKey();
-            int cols = entry.getValue()[0];
-            int rowsIns = entry.getValue()[1];
-            int rowsUpd = entry.getValue()[2];
-            KafkaProducer<Long, String> producer = new KafkaProducer<>(config);
-            StringBuilder sb = new StringBuilder();
+        long start = System.currentTimeMillis();
 
-            for (Long row = 0L; row < rowsIns; row++) {
-                producer.send(new ProducerRecord<Long, String>(topic, row, row(cols, sb)));
-            }
+        KafkaProducer<Integer, String> producer = new KafkaProducer<>(config);
+        StringBuilder sb = new StringBuilder();
+        Random rnd = new Random();
 
-            Random rnd = new Random();
-            for (Long row = 0L; row < rowsUpd; row++) {
-                producer.send(new ProducerRecord<Long, String>(topic, (long)rnd.nextInt(rowsIns), row(cols, sb)));
-            }
-
-            producer.close();
+        for (int topicNum = 0; topicNum < numTopics; topicNum++) {
+            String topic = topicNames + topicNum;
+            for (Integer row = 0; row < numInserts; row++)
+                producer.send(new ProducerRecord<>(topic, row, row(numColumns, sb)));
+            for (Integer row = 0; row < numUpdates; row++)
+                producer.send(new ProducerRecord<>(topic, rnd.nextInt(numInserts), row(numColumns, sb)));
         }
+        producer.close();
+
+        long now = System.currentTimeMillis();
+        System.out.println("Time: " + (now - start));
     }
 
-    /** @return
-     * int[0] cols
-     * int[1] rows insert
-     * int[2] rows update
-     */
-    public static Map<String, int[]> topics(Properties config) {
-        return Arrays.stream(config.getProperty("topics").split(",")).collect(
-                Collectors.toMap(
-                        x -> x.split(":")[0],
-                        x -> Arrays.stream(x.split(":")[1].split("x")).mapToInt(Integer::valueOf).toArray()
-                ));
-    }
-
-    private static Properties config() throws IOException {
+    private static Properties config(String[] args) throws IOException {
         Properties config = new Properties();
-        InputStream configStream = TestProducer.class.getResourceAsStream("config.properties");
+        InputStream configStream = new FileInputStream(args[0]); // TestProducer.class.getResourceAsStream("config.properties");
         config.load(configStream);
-        config.put("key.serializer", "org.apache.kafka.common.serialization.LongSerializer");
+        config.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
         config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         return config;
     }
